4b4aef299e1ed817a6db08a2d5e89bb5864cce17
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-07-14 16:39:50 -0700

    edited codes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  *main - print 0123456789
+ * *Description - this code prints digits 1 to 9 on separate lines.
+ *  *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k = 0;
+
+	while (k < 10)
+	{
+		printf("%d\n", k);
+		k++;
+	}
+	printf("\n");
+	return (0);
+}
