2272d652d177458e38c9d8fb2fa7a51ed80d87c1
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-07-15 01:44:21 -0700

    100 print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 /**
- *main - Entry point, print 00 to 99 using putchar
- *Return: Always 0 (Success)
+ * main - Entry point, print 00 to 99 using putchar, excluding same digit combinations
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int tens;
-	int ones;
+    int tens;
+    int ones;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
-	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
-		{
-			putchar(tens);
-			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
+    for (tens = '0'; tens <= '9'; tens++) /* print tens place */
+    {
+        for (ones = tens + 1; ones <= '9'; ones++) /* print ones place, starting from the next digit after tens */
+        {
+            putchar(tens);
+            putchar(ones);
+            if (tens != '8' || ones != '9') /* skip comma at the end */
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
 
-	return (0);
+    return 0;
 }
+
