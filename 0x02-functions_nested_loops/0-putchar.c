fdfa339182fbc746fe7e5cde1629f972ecbb095b
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-07-18 06:41:21 -0700

    putchar war

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar, followed by new line.
+ * Return: 0
+ */
+
+int _putchar()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
