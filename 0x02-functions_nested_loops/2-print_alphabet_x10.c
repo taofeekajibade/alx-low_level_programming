681fca3378c847834c5144b3c4a88fe07c7cb2e8
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-07-18 14:06:46 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10: print alphabets 10x
+ * Return: Always 0;
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	}
+	_putchar('\n');
+}
