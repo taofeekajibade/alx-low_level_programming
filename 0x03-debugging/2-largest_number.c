be2800764ca53145779e2650555ea2dae6c9ac7c
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-07-20 12:09:40 -0700

    edited for betty

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,19 +7,18 @@
 * @c: third integer
 * Return: the largest number among a, b, and c
 */
+
 int largest_number(int a, int b, int c)
 {
-    int largest = a;
+	int largest = a;
 
-    if (b > largest)
-    {
-        largest = b;
-    }
-
-    if (c > largest)
-    {
-        largest = c;
-    }
-
-    return largest;
+	if (b > largest)
+	{
+		largest = b;
+	}
+	if (c > largest)
+	{
+		largest = c;
+	}
+	return (largest);
 }
