8aea6f6c4162b59efc75fac43b601f7752a02e71
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-07 10:58:19 +0100

    edited code for mixed declaration error flag

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,20 +5,25 @@
  * create_array - create array of characters
  * @size: size of character
  * @c: character
+ * Return: 0
  */
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
-	char *arr = (char *)malloc(sizeof(char) * size);
+	arr = (char *)malloc(size * sizeof(char));
+
 	if (arr == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
