a8bfa02915a1425e580670aded1ea19a15d04514
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-12 03:17:11 +0100

    reformatted files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,93 +1,56 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#define ERR_MSG "Error"
+#include <stdlib.h>
+#include <unistd.h>
 /**
- * is_digit - checks non-digit char in a string
- * @s: string checked
- * Return: 0 for non-digit char or 1 for digit char
+ * mul - returns value of multiplication
+ * @num1: argument 1
+ * @num2: argument 2
+ * Return: total
  */
-int is_digit(char *s)
-{
-	int i = 0;
 
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-/**
- * _strlen - function for the length of a string
- * @s: string checked
- * Return: length of the string
- */
-int _strlen(char *s)
+int mul(int num1, int num2)
 {
-	int i = 0;
+	return (num1 * num2);
+}
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 /**
- * errors - checks for errors in main
+ * print_number - prints an integer to the standard output
+ * @n: the integer to print
  */
-void errors(void)
+void print_number(int n)
 {
-	printf("Error\n");
-	exit(98);
+	int div = 1;
+	int temp = n;
+
+	if (n < 0)
+		putchar('-');
+		n = -n;
+		temp = n;
+	while (temp / 10 > 0)
+		div *= 10;
+		temp /= 10;
+	while (div >= 1)
+		putchar(n / div + '0');
+		n %= div;
+		div /= 10;
 }
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0
+ * main - multiplies two numbers
+ * @argc: argument 1
+ * @argv: argument 1
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int num1, num2, result;
 
-	1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
-	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
+	if (argc != 3)
+		write(2, "Error\n", 6);
+		return (98);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = mul(num1, num2);
+	print_number(result);
+	putchar('\n');
 	return (0);
 }
