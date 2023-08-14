ba28e1132a6363bd226cbe443cae7ec835a72e36
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-14 17:55:23 +0100

    added initial files

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+0 - Define a new type struct dog with the following elements
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct - to represent information about a dog
+ * @struct dog: variable name
+ * @name: 1st element
+ * @age: 2nd element
+ * @owner: 3rd element
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* DOG_H */
