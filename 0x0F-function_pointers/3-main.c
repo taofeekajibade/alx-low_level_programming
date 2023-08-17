1bec2ed236f53d27606494fa4e2d95adb233f306
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-08-17 08:14:54 +0100

    edited to remove extra if

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,40 +9,27 @@ int main(int argc, char *argv[])
         int a, b, answer;
         int (*ptr)(int, int);
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                return 98;
-        }
-
-        operator = argv[2][0];
-        if ((operator != '*' && operator != '+' && operator != '-' &&
-             operator != '/' && operator != '%') || argv[2][1] != '\0')
-        {
-                printf("Error\n");
-                return 99;
-        }
-
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
-        if ((operator == '/' || operator == '%') && b == 0)
-        {
-                printf("Error\n");
-                return 100;
-        }
-
-        sign = argv[2];
-        ptr = get_op_func(sign);
-        if (!ptr)
-        {
-                printf("Error\n");
-                return 99;
-        }
-
-        answer = (*ptr)(a, b);
-        printf("%d\n", answer);
-
-	        return 0;
+	 if (argc != 4)
+	 {
+		 printf("Error\n");
+		 return (98);
+	 }
+	 a = atoi(argv[1]);
+	 b = atoi(argv[3]);
+	 if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	 {
+		 printf("Error\n");
+		 return (100);
+	 }
+	 ptr = get_op_func(argv[2]);
+	 if (!ptr)
+	 {
+		 printf("Error\n");
+		 return (99);
+	 }
+	 answer = ptr(a, b);
+	 printf("%d\n", answer);
+	 return (0);
 
 }
 
