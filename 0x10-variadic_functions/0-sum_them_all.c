541cab46a26c19d03787d38e4e239672c0ce26a6
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-08-18 05:47:48 +0100

    added README and prototype files

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - add all arguments togwther
+ * @n: number of arguments to sum
+ * Return: result
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+        int result = 0;
+        va_list list;
+        int i;
+
+        va_start(list, n);
+        for (i = 0; i < n; i++)
+                result += va_arg(list, int);
+	va_end(list);
+
+        return (result);
+
+}
