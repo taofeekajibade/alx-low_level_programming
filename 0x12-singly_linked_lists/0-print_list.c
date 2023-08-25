c84e63e0c7d4395a794ed9ab26da4e2b800038b9
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-08-25 03:39:47 -0700

    edited files for betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements of list_t list
- * @h: linked list
- * Return: number of nodes in the list
- */
+ * print_list - Prints all the elements of a list_t list
+ * @h: Head of the list
+ * Return: Numbers of elements
+ **/
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	int i;
 
-    while (h != NULL)
-    {
-        printf("%s\n", h->str == NULL ? "[0] (nil)" : h->str);
-        h = h->next;
-        count++;
-    }
+	if (h == NULL)
+		return (0);
 
-    return (count);
+	for (i = 0; h; i++)
+	{
+		printf("[%u] %s\n", (h->str != NULL) ? h->len : 0,
+				(h->str != NULL) ? h->str : "(nil)");
+		h = h->next;
+	}
+
+	return (i);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,15 @@
 #include "lists.h"
-
 /**
- * list_len - prints length of list_t linked list
- * @h: linked list
- * Return: number of nodes (length) in list
- */
-
+ * list_len - Returns the number of elements in a linked list_t list
+ * @h: Head of the list
+ * Return: Number of elements
+ **/
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	int i;
 
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
+	for (i = 0; h; i++, h = h->next)
+		;
 
-	return (count);
+	return (i);
 }
