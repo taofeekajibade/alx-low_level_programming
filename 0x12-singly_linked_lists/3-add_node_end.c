e6bba6c2df3cbd81d3b265d7f0fcd8a330283fa0
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-08-25 08:01:01 -0700

    edited sourced code for requirement error

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,34 @@
 #include "lists.h"
 /**
- * add_node_end - adds node at the end
- * @head:
- * @str:
+ * add_node_end - adds a new node to the end
+ * of list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
-    
-    if (str == NULL)
-    return (NULL);
-
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        list_t *current = *head;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = new_node;
-    }
-    return (new_node);
+	list_t *new_node, *current_node;
+	size_t n;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	for (n = 0; str[n]; n++)
+		;
+	new_node->len = n;
+	new_node->next = NULL;
+	current_node = *head;
+	if (current_node == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (current_node->next != NULL)
+			current_node = current_node->next;
+		current_node->next = new_node;
+	}
+	return (*head);
 
 }
-
