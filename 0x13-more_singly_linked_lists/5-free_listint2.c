f7b013285f390ac055787622d615938054f676b3
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-29 15:36:49 +0100

    added tasks 5 and 6 files

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the head node
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head nodes of a listint_t list
+ * *head: pointer to the head node
+ *
+ * Return: data from the old head node, or 0 if the list is empty
+ */
+int poplistint(listint_t **head)
+{
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	listint_t *node = *head;
+	int data = node->n;
+	*head = node->next;
+	free(node);
+
+	return (data);
+}
