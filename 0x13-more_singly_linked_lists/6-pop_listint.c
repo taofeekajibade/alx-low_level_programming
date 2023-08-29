b639c5506b25345b1ddc5dfcc0498584bc91deb9
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-29 16:13:45 +0100

    edited file for compiler error

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 /**
- * poplistint - deletes the head nodes of a listint_t list
+ * pop_listint - deletes the head nodes of a listint_t list
  * @head: pointer to the head node
  *
  * Return: data from the old head node, or 0 if the list is empty
  */
-int poplistint(listint_t **head)
+int pop_listint(listint_t **head)
 {
+
+	listint_t *node;
+	int data;
+
 	if (*head == NULL)
 	{
 		return (0);
 	}
 
-	listint_t *node = *head;
-	int data = node->n;
-	*head = node->next;
+	node = *head;
+	data = node->n;
+	*head = (*head)->next;
 	free(node);
 
 	return (data);
