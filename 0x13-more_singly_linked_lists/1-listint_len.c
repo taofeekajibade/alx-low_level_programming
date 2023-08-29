1f0d8b8bfcd2f91b43abd2836e61bfe36748a3e1
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-29 04:34:52 +0100

    added task 1 file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,3 +1,21 @@
-#include "main.h"
+#include "lists.h"
 
+/**
+ * listit_len - prints the number of elements in a linked listint_t list
+ * @h: pointer to nodes
+ *
+ * Return: number of element
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
 
