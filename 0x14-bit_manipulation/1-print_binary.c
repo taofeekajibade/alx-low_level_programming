86f9ea8eb74a6280337bc91208cd91044d64387d
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-01 03:00:56 +0100

    betty-checked code for task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,28 +4,27 @@
  * @n: num
  */
 
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int started = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int started = 0;
 
-    if (n == 0)
+	if (n == 0)
 	{
-        putchar('0');
-        return;
-    }
-
-    while (mask > 0)
+		putchar('0');
+		return;
+	}
+	while (mask > 0)
 	{
-        if (n & mask)
+		if (n & mask)
 		{
-            putchar('1');
-            started = 1;
-        }
-		else if (started) 
+			putchar('1');
+			started = 1;
+		}
+		else if (started)
 		{
-            putchar('0');
-        }
-        mask >>= 1;
-    }
+			putchar('0');
+		}
+		mask >>= 1;
+	}
 }
