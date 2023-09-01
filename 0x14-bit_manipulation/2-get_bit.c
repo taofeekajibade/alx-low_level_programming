6e14ba42c6f1e8460e3740c0276d689da7db5e90
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-01 02:41:57 +0100

    betty-checked code

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_bit - get bits 
+ * get_bit - get bits
  * @n: argument 1
  * @index: argument 2
  *
@@ -9,12 +9,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int mask = 1UL << index;
+	unsigned long int mask = 1UL << index;
 
 	if (index >= sizeof(unsigned long int) * 8)
-    {
-        return (-1); // Error: Invalid index
-    }
-    return (n & mask) ? 1 : 0;
+	{
+		return (-1); /* Error: Invalid index */
+	}
+	return ((n & mask) ? 1 : 0);
 }
 
