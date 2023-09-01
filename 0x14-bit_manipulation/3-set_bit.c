c9c4aa629aaefaeef25db8ca2748b6e77e227b6d
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-01 02:44:19 +0100

    betty-checked code for task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * set_bit - abc
- * @n: 123
- * @index: 123
- * Return: interns
+ * set_bit - function
+ * @n: argument 1
+ * @index: argument 2
+ * Return: int
+ *
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask;
+	unsigned long int mask;
 
-    if (index >= sizeof(unsigned long int) * 8)
-    {
-        return -1; // Error: Invalid index
-    }
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1); /* Error: Invalid index */
+	}
 
-    mask = 1UL << index;
-    *n |= mask;
-    return 1;
+	mask = 1UL << index;
+	*n |= mask;
+	return (1);
 }
