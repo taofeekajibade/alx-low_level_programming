c962fb4ab3f78ae60beb8064a3c89f5470c24ce9
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-05 04:21:44 +0100

    edited file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,38 @@
 #include "main.h"
 /**
- * create_file - create file
- * @filename: name of file
- * @text_content: content to read
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content written in the file.
+ *
+ * Return: 1 on success, or -1 if it fails.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-	{
-        return (-1);
-    }
+	int fd;
+	int nletters;
+	int rwr;
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
+	if (!filename)
 	{
-        return (-1);
-    }
-
-    if (text_content != NULL)
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 	{
-        ssize_t bytesWritten = write(fd, text_content, strlen(text_content));
-        if (bytesWritten == -1)
-		{
-            close(fd);
-            return (-1);
-        }
-    }
+		return (-1);
+	}
+	if (!text_content)
+	{
+		text_content = "";
+	}
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rwr = write(fd, text_content, nletters);
 
-    close(fd);
-    return (1);
+	if (rwr == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
