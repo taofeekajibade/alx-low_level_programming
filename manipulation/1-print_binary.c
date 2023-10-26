2fefe899a2e3ca7ed67642894105b2bbec3c97cb
taofeekajibade <cleargoal.ng@gmail.com>
2023-10-26 15:38:38 +0100

    added bit manipulation directory

---
+++ manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_binary - prints all
+ * @n: num
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int started = 0;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			putchar('1');
+			started = 1;
+		}
+		else if (started)
+		{
+			putchar('0');
+		}
+		mask >>= 1;
+	}
+}
